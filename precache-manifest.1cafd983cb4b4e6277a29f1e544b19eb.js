5826ea61ebe02d8ff5deb6d9ccf8a8a9de0391c7
samxtu <ngwalesamwel582@gmail.com>
2020-02-03 16:55:38 +0300

    Updates

---
+++ precache-manifest.1cafd983cb4b4e6277a29f1e544b19eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61056dfbd87ac5d50b8ab8df4cc1ee16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17f5cfcc85196dfc91f9",
+    "url": "/static/css/main.3147b840.chunk.css"
+  },
+  {
+    "revision": "f07e424c483949ed968b",
+    "url": "/static/js/2.c16d2ffe.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/static/js/2.c16d2ffe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17f5cfcc85196dfc91f9",
+    "url": "/static/js/main.88c53ac6.chunk.js"
+  },
+  {
+    "revision": "6b52fc38d1656a468902",
+    "url": "/static/js/runtime-main.c5ac5c46.js"
+  },
+  {
+    "revision": "294de88d56b7cd0f3231a8e53ea80e3a",
+    "url": "/static/media/no-image.294de88d.jpg"
+  }
+]);
